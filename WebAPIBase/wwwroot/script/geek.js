ca2ada48012447b1416df951fda0b9417378115f
ivvkcm <hosza.bendi@gmail.com>
2023-11-28 08:29:21 +0100

    a

---
+++ WebAPIBase/wwwroot/script/geek.js
@@ -0,0 +1,11 @@
+﻿var viccek;
+fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}
---
+++ pascal/wwwroot/scripts/pascaltemp.js
@@ -0,0 +1,32 @@
+﻿window.onload = function () {
+
+    drawPascal(10);
+}
+
+var drawPascal = function (sorSzam) {
+
+    for (var sor = 0; sor < 10; sor++)
+    {
+        var ujSorDIv = document.createElement('div');
+        ujSorDIv.classList.add(ujSorDiv);
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++)
+        {
+            var ujElemDiv = document.createElement('div');
+            ujElemDiv.classList.add('elem');
+            ujElemDiv.innerText =
+                faktorialis(sor) / faktorialis(oszlop) * faktorialis(sor - oszlop);
+            ujSorDIv.appendChild(ujElemDiv);
+
+        }
+    }
+}
+/*
+var ujElem = document.createElement('div');
+ujElem.classList.add('ujOsztaly');
+ujElem.id = 'ujID';
+ujElem.innerText = '0';
+ujElem.innerHTML = '<div>HEllo</div>';
+
+var szuloElem = document.getElementById('szulo');
+szuloElem.appendChild(ujElem);*/
